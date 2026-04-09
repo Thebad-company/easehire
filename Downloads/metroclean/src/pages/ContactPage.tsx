062c340400b225144fe5b50063a7ca300c758329
hardikinfabio <vivek@thebadcompany.co>
2026-04-09 18:13:53 +0530

    chore: remove tracked files from sibling directories

--- Downloads/metroclean/src/pages/ContactPage.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactSection } from "../components/ContactSection";
-
-export const ContactPage = () => {
-  return (
-    <main className="pt-32 pb-24 bg-background min-h-screen">
-      <ContactSection />
-    </main>
-  );
-};
